78240cb90fa70d9cf49c437e06edf47b7405c8d9
wnmb99 <49930283+wnmb99@users.noreply.github.com>
2020-04-24 20:29:09 -0600

    Program

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,52 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Nate Brown";
+            string location = "Utah";
+
+            Console.WriteLine($"{"My name is "}{name}");
+            Console.WriteLine($"{"I am from "}{location}");
+
+            DateTime theDate = new DateTime(2020, 4, 24, 0, 0, 0);
+
+            Console.WriteLine($"{"The date is "}{theDate.Date:MM/dd/yyyy}");
+
+
+            DateTime today = DateTime.Now;
+            DateTime christmas = new DateTime(2020, 12, 25);
+
+            int timeLeft = (christmas - today).Days;
+
+            Console.WriteLine($"{"There are "}{timeLeft}{" days until Christmas"}");
+
+            Console.WriteLine("\nPress any key to enter the program.");
+            Console.ReadKey();
+            double width, height, woodLength, glassArea;
+
+            string widthString, heightString;
+            Console.WriteLine("\nPlease enter the width in feet. ");
+            widthString = Console.ReadLine();
+
+            width = double.Parse(widthString);
+            Console.WriteLine("\nPlease enter the height in feet. ");
+            heightString = Console.ReadLine();
+
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("\nThe length of the wood is " +
+            woodLength + " feet");
+
+            Console.WriteLine("\nThe area of the glass is " +
+            glassArea + " square metres");
+
+            Console.WriteLine("\nPress any key to exit the program.");
+            Console.ReadKey();
+
+
         }
     }
 }
