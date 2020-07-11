5a5ed80f6dbec132fc0e652f14530f48f6e94000
wnmb99 <49930283+wnmb99@users.noreply.github.com>
2020-07-11 11:42:24 -0600

    MongoDB program

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+    {
+        new SelectListItem { Value = "DR", Text = "Drama" },
+        new SelectListItem { Value = "CO", Text = "Comedy" },
+        new SelectListItem { Value = "RC", Text = "Romantic Comedy"  },
+        new SelectListItem { Value = "DO", Text = "Documentary"  },
+        new SelectListItem { Value = "HI", Text = "Historical"  },
+    };
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven 2",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Biography",
+                        Rating = "PG-13",
+                        Price = 2.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-10-10"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 2.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Fighting Preacher",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Love Kennedy",
+                        ReleaseDate = DateTime.Parse("2017-6-2"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
